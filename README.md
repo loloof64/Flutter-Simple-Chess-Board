565e7fad483b8a7d4dcb7bb9dda72a4d7ceb7be4
Laurent Bernabe <laurent.bernabe@gmail.com>
2022-03-10 11:08:08 +0100

    Added a longer example, and completed readme.

--- README.md
+++ README.md
@@ -34,8 +34,7 @@ start using the package.
 
 ## Usage
 
-TODO: Include short and useful examples for package users. Add longer examples
-to `/example` folder. 
+You can find a longer example in the `example` folder. 
 
 ### Simple example
 
@@ -119,9 +118,7 @@ SimpleChessBoard(
 
 ## Additional information
 
-TODO: Tell users more about the package: where to find more information, how to 
-contribute to the package, how to file issues, what response they can expect 
-from the package authors, and more.
+You can find the repository on [Github](https://github.com/loloof64/Flutter-Simple-Chess-Board).
 
 ## Credits
 
---
+++ example/lib/main.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+import 'package:simple_chess_board/models/board_arrow.dart';
+import 'package:chess/chess.dart' as chesslib;
+import 'package:simple_chess_board/simple_chess_board.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Simple chess board Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Simple chess board Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final _chess = chesslib.Chess.fromFEN(chesslib.Chess.DEFAULT_POSITION);
+  var _blackAtBottom = false;
+  BoardArrow? _lastMoveArrowCoordinates;
+
+  void tryMakingMove({required ShortMove move}) {
+    final success = _chess.move(<String, String?>{
+      'from': move.from,
+      'to': move.to,
+      'promotion': move.promotion.match(
+        (piece) => piece.name,
+        () => null,
+      ),
+    });
+    if (success) {
+      setState(() {
+        _lastMoveArrowCoordinates =
+            BoardArrow(from: move.from, to: move.to, color: Colors.greenAccent);
+      });
+    }
+  }
+
+  Future<PieceType?> handlePromotion(BuildContext context) {
+    final navigator = Navigator.of(context);
+    return showDialog<PieceType>(
+      context: context,
+      builder: (_) {
+        return AlertDialog(
+          title: const Text('Promotion'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ListTile(
+                title: const Text("Queen"),
+                onTap: () => navigator.pop(PieceType.queen),
+              ),
+              ListTile(
+                title: const Text("Rook"),
+                onTap: () => navigator.pop(PieceType.rook),
+              ),
+              ListTile(
+                title: const Text("Bishop"),
+                onTap: () => navigator.pop(PieceType.bishop),
+              ),
+              ListTile(
+                title: const Text("Knight"),
+                onTap: () => navigator.pop(PieceType.knight),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final boardOrientation =
+        _blackAtBottom ? BoardColor.black : BoardColor.white;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        actions: [
+          IconButton(
+            onPressed: () {
+              setState(() {
+                _blackAtBottom = !_blackAtBottom;
+              });
+            },
+            icon: const Icon(Icons.swap_vert),
+          )
+        ],
+      ),
+      body: Center(
+        child: SimpleChessBoard(
+          engineThinking: false,
+          fen: _chess.fen,
+          onMove: tryMakingMove,
+          orientation: boardOrientation,
+          whitePlayerType: PlayerType.human,
+          blackPlayerType: PlayerType.human,
+          lastMoveToHighlight: _lastMoveArrowCoordinates,
+          onPromote: () => handlePromotion(context),
+        ),
+      ),
+    );
+  }
+}
